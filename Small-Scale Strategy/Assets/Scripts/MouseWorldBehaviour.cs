3e3043b6d0c85425fcd08c4cd481652e4ba1e418
CalvinRA <10659444@my.uvu.edu>
2024-03-02 23:09:32 -0700

    Animation Tutorial Work

--- Small-Scale Strategy/Assets/Scripts/MouseWorldBehaviour.cs
+++ Small-Scale Strategy/Assets/Scripts/MouseWorldBehaviour.cs
@@ -1,20 +1,19 @@
-using System;
 using UnityEngine;
 
 public class MouseWorldBehaviour : MonoBehaviour
 {
     private static MouseWorldBehaviour instance;
-    [SerializeField] private LayerMask mousePlaneLyerMask;
+    [SerializeField] private LayerMask mousePlaneLayerMask;
 
     private void Awake()
     {
         instance = this;
     }
     
-    public static Vector3 GetPoaition()
+    public static Vector3 GetPosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLyerMask);
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLayerMask);
         return raycastHit.point;
         
     }
--- Small-Scale Strategy/Assets/Scripts/UnitBehaviour.cs
+++ Small-Scale Strategy/Assets/Scripts/UnitBehaviour.cs
@@ -1,23 +1,31 @@
 using UnityEngine;
-using UnityEngine.Events;
 
 public class UnitBehaviour : MonoBehaviour
 {
+
+    [SerializeField] private Animator UnitAnimator;
     private Vector3 targetPosition;
 
     private void Update()
-    {
+    {   
+        
         float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            
+            UnitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            UnitAnimator.SetBool("IsWalking", false);
         }
         
         if (Input.GetMouseButtonDown(0))
         {
-            Move(MouseWorldBehaviour.GetPoaition());
+            Move(MouseWorldBehaviour.GetPosition());
         }
     }
 
